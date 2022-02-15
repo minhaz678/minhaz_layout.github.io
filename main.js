273df73080bff927862d6a542fe6523da68848dd
minhaz678 <79627378+minhaz678@users.noreply.github.com>
2022-02-15 21:05:00 +0600

    js

---
+++ main.js
@@ -0,0 +1,8 @@
+var greeting = prompt("What's your name?");
+alert("Good Evening, " + greeting);
+window.alert("Welcome to Minhaz's addition calculator");
+var first = prompt("Enter your first number!");
+var second = prompt("Enter your second number!");
+var sum = Number(first) + Number(second);
+alert("The sum is " + sum);
+alert("Have a nice day!, dear " + greeting);
